03600b314f9664083781736b9a8436cf5ee7b58f
ttt318 <vlade8422@gmail.com>
2025-02-20 16:33:57 +0300

    p

---
+++ project/Part 13/enemy_data.py
@@ -0,0 +1,126 @@
+ENEMY_SPAWN_DATA = [
+  {
+    #1
+    "weak": 15,
+    "medium": 0,
+    "strong": 0,
+    "elite": 0
+  },
+  {
+    #2
+    "weak": 30,
+    "medium": 0,
+    "strong": 0,
+    "elite": 0
+  },
+  {
+    #3
+    "weak": 20,
+    "medium": 5,
+    "strong": 0,
+    "elite": 0
+  },
+  {
+    #4
+    "weak": 30,
+    "medium": 15,
+    "strong": 0,
+    "elite": 0
+  },
+  {
+    #5
+    "weak": 5,
+    "medium": 20,
+    "strong": 0,
+    "elite": 0
+  },
+  {
+    #6
+    "weak": 15,
+    "medium": 15,
+    "strong": 4,
+    "elite": 0
+  },
+  {
+    #7
+    "weak": 20,
+    "medium": 25,
+    "strong": 5,
+    "elite": 0
+  },
+  {
+    #8
+    "weak": 10,
+    "medium": 20,
+    "strong": 15,
+    "elite": 0
+  },
+  {
+    #9
+    "weak": 15,
+    "medium": 10,
+    "strong": 5,
+    "elite": 0
+  },
+  {
+    #10
+    "weak": 0,
+    "medium": 100,
+    "strong": 0,
+    "elite": 0
+  },
+  {
+    #11
+    "weak": 15,
+    "medium": 18,
+    "strong": 12,
+    "elite": 3
+  },
+  {
+    #12
+    "weak": 20,
+    "medium": 20,
+    "strong": 15,
+    "elite": 7
+  },
+  {
+    #13
+    "weak": 28,
+    "medium": 0,
+    "strong": 30,
+    "elite": 15
+  },
+  {
+    #14
+    "weak": 20,
+    "medium": 20,
+    "strong": 20,
+    "elite": 20
+  },
+  {
+    #15
+    "weak": 50,
+    "medium": 50,
+    "strong": 50,
+    "elite": 50
+  }
+]
+
+ENEMY_DATA = {
+    "weak": {
+    "health": 10,
+    "speed": 3
+  },
+    "medium": {
+    "health": 15,
+    "speed": 4
+  },
+    "strong": {
+    "health": 20,
+    "speed": 5
+  },
+    "elite": {
+    "health": 30,
+    "speed": 7
+  }
+}
